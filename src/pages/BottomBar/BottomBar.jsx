e8f28db03fe0fd2ae962f4e310be0830e20510e4
Dhairya99999 <dhairya.jan9@gmail.com>
2024-09-19 12:11:28 +0530

    call

--- src/pages/BottomBar/BottomBar.jsx
+++ src/pages/BottomBar/BottomBar.jsx
@@ -16,22 +16,14 @@ import { useNavigate } from "react-router-dom";
 const BottomBar = () => {
 	const navigate = useNavigate();
 
-    const handleCallJustdial = () => {
-        const phoneNumber = "+919888417555";
-        window.location.href = `tel:${phoneNumber}`;
-    };
-
-    useEffect(() => {
-        const handleVisibilityChange = () => {
-            if (document.visibilityState === 'visible') {
-                navigate("/");
-            }
-        };
-        document.addEventListener('visibilitychange', handleVisibilityChange);
-        return () => {
-            document.removeEventListener('visibilitychange', handleVisibilityChange);
-        };
-    }, [navigate]);
+	// Function to handle phone call initiation
+	const handleCallJustdial = () => {
+			 const phoneNumber = "+919888417555";
+			// console.log(`Dialing: ${phoneNumber}`); // Log the number
+			 window.location.href = `tel:${phoneNumber}`;
+			// Redirect back to home page
+			navigate("/");
+	};
 
 	return (
 		<>
